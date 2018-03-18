467cf9af5e75bd389616c42280a38b9625beb6ef
Jean-Raymond Raynal <jeanraymond.raynal@gmail.com>
2018-03-18 10:27:18 -0400

    more compilation errors

--- config.def.h
+++ config.def.h
@@ -83,10 +83,10 @@ char *termname = "st-256color";
 unsigned int tabspaces = 8;
 
 /* bg opacity */
-unsigned int alpha = 0xcc;
+unsigned int alpha = 0x00;
 
 /* Terminal colors (16 first used in escape sequence) */
-const char *colorname[] = {
+static const char *colorname[] = {
 	/* 8 normal colors */
 	"black",
 	"red3",
